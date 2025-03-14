06baf03c73bb86b367ec294f4695d53fb3a0b641
HenriClopes <henriclopes@gmail.com>
2025-03-14 14:24:45 -0300

    arrumei os sinais

--- script.js
+++ script.js
@@ -1,210 +1,26 @@
-/*
-
-
-let valorNaTela = 0;
-    decimal = false,
-    segNum = false,
-    operador = "",
-    lstN1 = [],
-    lstN2 = [];
-
-
-
-function zerar(){
-
-    valorNaTela = '0', 
-    operacao = false, 
-    operando = false;
-    decimal = false;
-    lst = ["0","0","0","0"];
-
-    document.getElementById("resultado").innerHTML = valorNaTela;
-    
-}
-function mostrarTela(y){
-
-    if (valorNaTela == "0") {
-
-        valorNaTela = String(y);
-
-        document.getElementById("resultado").innerHTML = valorNaTela;
-
-    }
-    else{
-
-        valorNaTela = valorNaTela + String(y);
-
-        document.getElementById("resultado").innerHTML = valorNaTela;
-
-    }
-
-    if (operando == true){
-
-        var umaVez = true;
-
-        if (umaVez){
-
-            decimal = false;
-
-            umaVez = false;
-
-        }
-
-        if (decimal == false){
-
-            lst[2] = lst[2] + y;
-
-
-        }
-        else{
-
-            lst[3] = lst[3] + y;
-
-        }
-
-    }
-    else{
-
-        if (decimal == false){
-
-            lst[0] = lst[0] + y;
-
-
-        }
-        else{
-
-            lst[1] = lst[1] + y;
-
-        }
-
-    }
-
-}
-function pegarOperador(y){
-
-    if (operando == false){
-
-        valorNaTela = valorNaTela + y
-
-        document.getElementById("resultado").innerHTML = valorNaTela;
-
-        operando = true;
-
-        operador = y;
-
-    }
-
-}
-function tornarDecimal(ponto){
-
-    if (decimal == false){
-
-        valorNaTela = valorNaTela + ponto;
-    
-        document.getElementById("resultado").innerHTML = valorNaTela;
-    
-        decimal = true;
-
-    }
-
-}
-function calculo(operador){
-
-    if (operador == "+"){
-
-        console.log(lst)
-
-
-
-        let n1 = Number(lst[0]);
-        let n2 = Number(lst[1]);
-
-
-
-        valorNaTela = n1 + n2;
-
-        operando = mostrarResultado();
-
-    }
-    else if (operador == "-"){
-
-        let n1 = Number(lst[0]);
-        let n2 = Number(lst[1]);
-
-        valorNaTela = n1 - n2;
-
-        operando = mostrarResultado();
-        
-    }
-    else if (operador == "x"){
-
-        let n1 = Number(lst[0]);
-        let n2 = Number(lst[1]);
-
-        valorNaTela = n1 * n2;
-
-        operando = mostrarResultado();
-        
-    }
-    else if (operador == "/"){
-
-        let n1 = Number(lst[0]);
-        let n2 = Number(lst[1]);
-
-        valorNaTela = n1 / n2;
-
-        operando = mostrarResultado();;
-        
-    }
-    else if (operador == "%"){
-
-        let n1 = Number(lst[0]);
-        let n2 = Number(lst[1]);
-
-        valorNaTela = n1 % n2;
-
-        operando = mostrarResultado();
-
-    }
-
-}
-function mostrarResultado(){
-
-    document.getElementById("resultado").innerHTML = String(valorNaTela);
-
-    lst = [String(valorNaTela),"0"];
-
-    console.log(lst)
-
-    return false;
-
-}
-
-let valorNaTela = "0",
-    operador = " ", 
-    operando = false,
-    decimal = false,
-    lst = ["0","0","0","0"];*/
-
 let n1 = "",
     n2 = "",
     operador = "",
-    segNum = false,
+    resultado = "",
+    valorNaTela = 0,
     operacao = false,
-    lst = [[],operador,[]];
+    decimal = false,
+    inicio = false,
+    lst = [[""],operador,[""]];
 
 function addLista(x){
+
+    inicio = true;
 
     if (operacao){
     
         lst[2].push(x);
         
         mostrarNaTela(lst[0], operador, lst[2]);
-
     
     }
     else{
-    
+
         lst[0].push(x);
         
         mostrarNaTela(lst[0], operador, lst[2]);
@@ -217,106 +33,270 @@ function mostrarNaTela(x, y, z){
     document.getElementById("resultado").innerHTML = valorNaTela;
     
 }
+function escOperação(x){
+
+    if (x == "+"){
+
+        operacao = true;
+    
+        decimal = false;
+
+        mostrarNaTela(lst[0], x, lst[2]);
+    
+        return "+";
+
+    }
+    else if (x == "-"){
+
+        operacao = true;
+    
+        decimal = false;
+
+        mostrarNaTela(lst[0], x, lst[2]);
+
+        return "-";
+
+    }
+    else if (x == "x"){
+
+        operacao = true;
+    
+        decimal = false;
+
+        mostrarNaTela(lst[0], x, lst[2]);
+
+        return "x";
+
+    }
+    else if (x == "/"){
+
+        operacao = true;
+    
+        decimal = false;
+
+        mostrarNaTela(lst[0], x, lst[2]);
+
+        return "/";
+
+    }
+    else if (x == "%"){
+
+        operacao = true;
+    
+        decimal = false;
+
+        mostrarNaTela(lst[0], x, lst[2]);
+
+        return "%";
+
+    }
+
+}
 function pegarOperador(x){
 
-    if(operacao){
+    if (x == "-" && operacao == false){
 
-        calculo();
+        lst[0].unshift(x);
+
+        mostrarNaTela(lst[0], operador, lst[2]);
 
     }
+    if (inicio){
 
-    if (valorNaTela != ""){
+        if(operacao){
 
-        if (x == "+"){
-
-            operador = x;
-
-            operacao = true;
-
-            mostrarNaTela(lst[0], operador, lst[2]);
+            calculo();
     
         }
+    
+        if (lst[0].slice(-1) == "."){
+    
+            lst[0].pop()
+    
+            operador = escOperação(x);
+    
+        }
+        else{
+    
+            operador = escOperação(x);
+    
+        }
+
     }
+
+}
+function posCalculo(x){
+
+    var lista = String(x).split("");
+    
+    lista.unshift(" ");
+
+    operador = " ";
+
+    lst = [lista, operador,[""]]
+    
+    decimal = false;
+
+    operacao = false;
+
+    document.getElementById("resultado").innerHTML = x;
 
 }
 function calculo(){
 
-    n1 = Number(lst[0].join(""));
-    n2 = Number(lst[2].join(""));
+    if (inicio){
 
-    if (operador == "+"){
+        n1 = Number(lst[0].join(""));
+        n2 = Number(lst[2].join(""));
+    
+        if (operador == "+"){
+    
+            resultado = n1 + n2;
 
-        var soma = n1 + n2;
+            posCalculo(resultado);
+    
+        }
+        else if (operador == "-"){
+
+            resultado = n1 - n2;
+
+            posCalculo(resultado);
+
+        }
+        else if (operador == "x"){
+
+            resultado = n1 * n2;
+
+            posCalculo(resultado);
+
+        }
+        else if (operador == "/"){
+
+            resultado = n1 / n2;
+
+            posCalculo(resultado);
+
+        }
+        else if (operador == "%"){
 
 
-        document.getElementById("resultado").innerHTML = soma;
+            resultado = n1 % n2;
+
+            posCalculo(resultado);
+
+        }
+
+    }
+
+}   
+function tornarDecimal(y){
+
+    if (operacao){
+
+        if(decimal == false){
+
+            if (lst[2] == ""){
+
+                lst[2].push(0);
+                lst[2].push(y);
+        
+                mostrarNaTela(lst[0], operador, lst[2])
+        
+                decimal = true;
+
+            }
+            else{
+
+                lst[2].push(y);
+        
+                mostrarNaTela(lst[0], operador, lst[2])
+        
+                decimal = true;
+
+            }
+
+        }
+
+    }
+    else{
+
+        if (decimal == false){
+
+            if (valorNaTela == 0){
+
+                lst[0].push(0);
+                lst[0].push(y);
+        
+                mostrarNaTela(lst[0], operador, lst[2])
+        
+                decimal = true;
+
+            }
+            else{
+
+                lst[0].push(y);
+        
+                mostrarNaTela(lst[0], operador, lst[2])
+        
+                decimal = true;
+
+            }
+
+        }
 
     }
 
 }
+function zerar(){
 
+    if (inicio){
 
+        if (operacao){
 
+            console.log(lst[1].length ,lst[2].length)
 
+            if (operacao && lst[2].length == 1){
 
+                operador = ""
 
+                operacao = false;
+                
+                mostrarNaTela(lst[0], operador, lst[2]);
 
-function verifEstList(x, y){
-
-    if (x[x.length - 1] != y){
-
-
-        if (y == Number){
-
-            addLista(y);
+            }
+    
+            lst[2].pop()
+            
+            mostrarNaTela(lst[0], operador, lst[2]);
 
         }
         else{
+            if (lst[0].length <= 2){
 
-            return y;
+                n1 = "",
+                n2 = "",
+                operador = "",
+                resultado = "",
+                valorNaTela = 0,
+                operacao = false,
+                decimal = false,
+                inicio = false,
+                lst = [[""],operador,[""]];
+
+                document.getElementById("resultado").innerHTML = 0;
+
+            }
+            else{
+
+                console.log(operador)
+                lst[0].pop();
+                console.log(lst[0])
+                mostrarNaTela(lst[0], operador, lst[2]);
+
+            }
 
         }
 
-
     }
 
 }
-
-function addDecimalList(y){
-
-    if (segNum){
-
-        var x = verifEstList(lstN2, y);
-
-        lstN2.push(x);
-
-    }
-    else{
-
-        verifEstList(lstN1, y);
-
-        var x = verifEstList(lstN2, y);
-
-        lstN1.push(x);
-
-    }
-
-}
-function tornarDecimal(y){
-
-    if (valorNaTela != ""){
-
-        addDecimalList(y);
-
-    }
-    else{
- 
-        lstN1.unshift(0);
-
-        addDecimalList(y);
-
-    }    
-
-}
-
-
